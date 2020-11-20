1f73b11196ef35d5ce959b216fb287497857e3b7
ChristianGSH <74607062+ChristianGSH@users.noreply.github.com>
2020-11-20 15:21:58 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+
+
+
+</head>
+
+
+
+<body>
+    <div class='container'>
+
+        <p>Copyright &copy; 2020 Christian</p>
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
